5c262c7d96053dac368ed1275e2069adbb6f7100
askfiqmaster@gmail.com <askfiqmaster@gmail.com>
2025-10-18 23:39:58 +0700

    image on catch error

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 fetch(
-  'https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature'
+  'https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=asdgsgsrg'
 )
   .then((res) => res.json())
   .then((data) => {
-    console.log(data);
+    console.log(data.urls.regular);
 
     document.body.style.backgroundImage = `url(${data.urls.regular})`;
     document.getElementById('author-name').innerText = `By: ${data.user.name}`;
@@ -11,13 +11,6 @@ fetch(
     //   'author-img'
     // ).innerHTML = `<img src=${data.profile_image.large})>`;
   })
-  .catch((err) => console.error('something went wrong!!', err));
-
-/**
- * Challenge: get a URL for a default background image and set it here
- *
- * 1. Change the query in the URL above back to something real
- * 2. Log the image url to the console (replacing console.log(data) above)
- * 3. Use that URL as the "default" background image to be used if
- *    the promise is ever rejected.
- */
+  .catch((err) => {
+    document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1596825113842-9c559495f65b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NjA4MDU1MDJ8&ixlib=rb-4.1.0&q=80&w=1080)`;
+  });
