1738a3e10d2056c2e7c7e810748584127085f326
askfiqmaster@gmail.com <askfiqmaster@gmail.com>
2025-10-18 11:14:16 +0700

    adding author name

--- script.js
+++ script.js
@@ -5,5 +5,19 @@ fetch(
   .then((data) => {
     console.log(data);
 
-    document.body.style.backgroundImage = `url(${data.urls.full})`;
+    document.body.style.backgroundImage = `url(${data.urls.regular})`;
+    document.getElementById('author-name').innerText = `By: ${data.user.name}`;
+    document.getElementById(
+      'author-img'
+    ).innerHTML = `<img src=${data.profile_image.large})>`;
   });
+
+/**
+ * Challenge part 2: Display the image's author
+ *
+ * With the data you've already fetched, display the name of the
+ * image author on the page. They show up as the "user" in the data
+ * returned from the API.
+ *
+ * Don't worry about positioning the author in the lower-left yet.
+ */
