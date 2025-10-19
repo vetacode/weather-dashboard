f64729cc42c7122848a81d147dd2c9d4e54a46b9
askfiqmaster@gmail.com <askfiqmaster@gmail.com>
2025-10-19 23:58:59 +0700

    add flex-shrink 0 on the image author

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ try {
   ).textContent = `Image By: ${data.user.name}`;
   document.getElementById(
     'author-img'
-  ).innerHTML = `<img src=${data.user.profile_image.medium}>`;
+  ).innerHTML = `<img src=${data.user.profile_image.small}>`;
 } catch (err) {
   document.body.style.backgroundImage = `url(https://plus.unsplash.com/premium_photo-1682310096066-20c267e20605?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2112)`;
   document.getElementById(
