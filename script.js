8cf4c02bc382c70f24b221e83bf9b4a874e9fc39
askfiqmaster@gmail.com <askfiqmaster@gmail.com>
2025-10-19 22:58:04 +0700

    fixing the font sizes of center elements

--- script.js
+++ script.js
@@ -6,7 +6,9 @@ try {
   const data = await res.json();
   console.log(data);
   document.body.style.backgroundImage = `url(${data.urls.full})`;
-  document.getElementById('author-name').textContent = `By: ${data.user.name}`;
+  document.getElementById(
+    'author-name'
+  ).textContent = `Image By: ${data.user.name}`;
 } catch (err) {
   document.body.style.backgroundImage = `url(https://plus.unsplash.com/premium_photo-1682310096066-20c267e20605?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2112)`;
   document.getElementById(
