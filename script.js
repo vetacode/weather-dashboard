259a7ef263400aad3c60f9838b46dc792bf7e19e
askfiqmaster@gmail.com <askfiqmaster@gmail.com>
2025-10-19 08:56:57 +0700

    checking error status

--- script.js
+++ script.js
@@ -12,13 +12,23 @@
 //     document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1502790671504-542ad42d5189?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NjA4MDU4MDd8&ixlib=rb-4.1.0&q=80&w=1080)`;
 //   });
 
-fetch(
-  'https://api.coingecko.com/api/v3/coins/bitcoin'
+fetch('https://api.coingecko.com/api/v3/coins/bitcoin')
   // ?tickers=true&market_data=true&community_data=true&developer_data=true&sparkline=true
-)
+  // "https://httpstat.us/200"
   .then((res) => res.json())
   .then((data) => {
     throw Error('XXX this is an Error');
     console.log(data);
   })
   .catch((err) => console.error('XXX The data is unavailable', err));
+
+// fetch('https://httpstat.us/200')
+//   .then((res) => {
+//     console.log(res.ok);
+//     console.log(res.status);
+//     return res.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((err) => console.error(err));
