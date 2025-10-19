b70bcb72082f8debd8d70b325afd392cf3bf5fa4
askfiqmaster@gmail.com <askfiqmaster@gmail.com>
2025-10-19 08:33:00 +0700

    adding crypto

--- script.js
+++ script.js
@@ -7,16 +7,14 @@ fetch(
 
     document.body.style.backgroundImage = `url(${data.urls.regular})`;
     document.getElementById('author-name').innerText = `By: ${data.user.name}`;
-    // document.getElementById(
-    //   'author-img'
-    // ).innerHTML = `<img src=${data.profile_image.large})>`;
   })
   .catch(() => {
     document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1502790671504-542ad42d5189?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3NjA4MDU4MDd8&ixlib=rb-4.1.0&q=80&w=1080)`;
   });
 
 fetch(
-  'https://api.coingecko.com/api/v3/simple/price?ids=BTC%2C%20ETH%2C%20DOGE%2C%20QNT&vs_currencies=USDT&include_market_cap=false&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=false&precision=2'
+  'https://api.coingecko.com/api/v3/coins/bitcoin?tickers=true&market_data=true&community_data=true&developer_data=true&sparkline=true'
 )
   .then((res) => res.json())
-  .then((data) => console.log(data));
+  .then((data) => console.log(data))
+  .catch((err) => console.log('The data is unavailable', err));
