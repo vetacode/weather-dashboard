03517de27a17ddcbdcbb3858e21ee6e2bbade771
askfiqmaster@gmail.com <askfiqmaster@gmail.com>
2025-10-19 21:20:46 +0700

    challenge convertin to await module

--- script.js
+++ script.js
@@ -1,42 +1,36 @@
-fetch(
+const res = await fetch(
   'https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature'
-)
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data.urls.regular);
+);
 
-    document.body.style.backgroundImage = `url(${data.urls.regular})`;
-    document.getElementById('author-name').innerText = `By: ${data.user.name}`;
-  })
-  .catch(() => {
-    document.body.style.backgroundImage = `url(https://plus.unsplash.com/premium_photo-1682310096066-20c267e20605?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2112)`;
-  });
+const data = await res.json();
+document.body.style.backgroundImage = `url(${data.urls.regular})`;
+document.getElementById('author-name').innerText = `By: ${data.user.name}`;
 
-fetch('https://api.coingecko.com/api/v3/coins/bitcoin')
-  .then((res) => {
-    if (!res.ok) {
-      document.body.style.backgroundImage = `url(https://media.istockphoto.com/id/670634464/id/foto/closeup-pengusaha-wanita-tangan-memegang-tanda-kartu-putih-dengan-di-bawah-konstruksi-pesan-teks.jpg?s=2048x2048&w=is&k=20&c=jfENiKwbPUi8HKPMAcsVGEuNUZ3LKgOk-Yh8E5v8sk8=)`;
-      throw Error('XXX this is an Error');
-    }
-    console.log(res.status);
-    return res.json();
-  })
-  .then((data) => {
-    console.log(data);
-    document.getElementById(
-      'crypto-name'
-    ).innerHTML = `<img src=${data.image.small} alt=${data.name}>
-    <span>${data.name}</span>
-    
-    `;
+// .catch((err) => {
+//   document.body.style.backgroundImage = `url(https://plus.unsplash.com/premium_photo-1682310096066-20c267e20605?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2112)`;
+// });
 
-    document.getElementById('crypto-price').innerHTML = `
-    <p>🎯: $${data.market_data.current_price.usd.toLocaleString('en-US')}</p>
-    <p>👆: $${data.market_data.high_24h.usd.toLocaleString('en-US')}</p>
-    <p>👇: $${data.market_data.low_24h.usd.toLocaleString('en-US')}</p>
-    `;
-  })
-  .catch((err) => console.error('XXX The data is unavailable', err));
+const res = await fetch('https://api.coingecko.com/api/v3/coins/bitcoin');
+if (!res.ok) {
+  document.body.style.backgroundImage = `url(https://media.istockphoto.com/id/670634464/id/foto/closeup-pengusaha-wanita-tangan-memegang-tanda-kartu-putih-dengan-di-bawah-konstruksi-pesan-teks.jpg?s=2048x2048&w=is&k=20&c=jfENiKwbPUi8HKPMAcsVGEuNUZ3LKgOk-Yh8E5v8sk8=)`;
+  throw Error('XXX this is an Error');
+}
+return res.json();
+const data = await res.json();
+document.getElementById(
+  'crypto-name'
+).innerHTML = `<img src=${data.image.small} alt=${data.name}>
+<span>${data.name}</span>
+
+`;
+
+document.getElementById('crypto-price').innerHTML = `
+<p>🎯: $${data.market_data.current_price.usd.toLocaleString('en-US')}</p>
+<p>👆: $${data.market_data.high_24h.usd.toLocaleString('en-US')}</p>
+<p>👇: $${data.market_data.low_24h.usd.toLocaleString('en-US')}</p>
+`;
+
+// .catch((err) => console.error('XXX The data is unavailable', err));
 
 function getCurrentTime() {
   const now = new Date();
@@ -103,27 +97,6 @@ navigator.geolocation.getCurrentPosition((position) => {
       document.getElementById('city').innerHTML = `
       <p>${data.name}</p>            
       `;
-    })
-    .catch((err) => console.error('There is something wrong', err));
+    });
+  // .catch((err) => console.error('There is something wrong', err));
 });
-
-// const options = {
-//   enableHighAccuracy: true,
-//   timeout: 5000,
-//   maximumAge: 0,
-// };
-
-// function success(pos) {
-//   const crd = pos.coords;
-
-//   console.log('Your current position is:');
-//   console.log(`Latitude : ${crd.latitude}`);
-//   console.log(`Longitude: ${crd.longitude}`);
-//   console.log(`More or less ${crd.accuracy} meters.`);
-// }
-
-// function error(err) {
-//   console.warn(`ERROR(${err.code}): ${err.message}`);
-// }
-
-// navigator.geolocation.getCurrentPosition(success, error, options);
