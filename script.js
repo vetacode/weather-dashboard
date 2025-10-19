06db2d87f96ea9615e47de2d9a40ed3790585022
askfiqmaster@gmail.com <askfiqmaster@gmail.com>
2025-10-19 17:42:05 +0700

    add navigator geolocation

--- script.js
+++ script.js
@@ -75,3 +75,33 @@ setInterval(getCurrentTime, 1000);
 //     console.log(data);
 //   })
 //   .catch((err) => console.error(err));
+
+navigator.geolocation.getCurrentPosition().then((position) => {
+  console.log(position);
+});
+
+// async function geolocation() {
+//   const position = await navigator.geolocation.getCurrentPosition();
+//   console.log(position);
+// }
+
+// const options = {
+//   enableHighAccuracy: true,
+//   timeout: 5000,
+//   maximumAge: 0,
+// };
+
+// function success(pos) {
+//   const crd = pos.coords;
+
+//   console.log('Your current position is:');
+//   console.log(`Latitude : ${crd.latitude}`);
+//   console.log(`Longitude: ${crd.longitude}`);
+//   console.log(`More or less ${crd.accuracy} meters.`);
+// }
+
+// function error(err) {
+//   console.warn(`ERROR(${err.code}): ${err.message}`);
+// }
+
+// navigator.geolocation.getCurrentPosition(success, error, options);
