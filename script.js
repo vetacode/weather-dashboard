a4c1d09123dcdee8e2b0823130db38b3b1506dfb
askfiqmaster@gmail.com <askfiqmaster@gmail.com>
2025-10-19 22:06:03 +0700

    convert to try catch

--- script.js
+++ script.js
@@ -9,30 +9,37 @@ try {
   document.getElementById('author-name').textContent = `By: ${data.user.name}`;
 } catch (err) {
   document.body.style.backgroundImage = `url(https://plus.unsplash.com/premium_photo-1682310096066-20c267e20605?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2112)`;
-  document.getElementById('author-name').textContent = `By: Vetacode`;
+  document.getElementById(
+    'author-name'
+  ).textContent = `Failed to Load the Image, Please Refresh the Page!`;
 }
 
-const res = await fetch('https://api.coingecko.com/api/v3/coins/bitcoin');
-if (!res.ok) {
-  document.body.style.backgroundImage = `url(https://media.istockphoto.com/id/670634464/id/foto/closeup-pengusaha-wanita-tangan-memegang-tanda-kartu-putih-dengan-di-bawah-konstruksi-pesan-teks.jpg?s=2048x2048&w=is&k=20&c=jfENiKwbPUi8HKPMAcsVGEuNUZ3LKgOk-Yh8E5v8sk8=)`;
-  throw Error('XXX this is an Error');
-}
+try {
+  const res = await fetch('https://api.coingecko.com/api/v3/coins/bitcoin');
+  if (!res.ok) {
+    document.body.style.backgroundImage = `url(https://plus.unsplash.com/premium_photo-1682310093719-443b6fe140e8?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=2112)`;
+    document.getElementById(
+      'author-name'
+    ).textContent = `Failed to Load Crypto Information, Please Reload the Page!`;
+    throw Error('XXX this is an Error');
+  }
 
-const data = await res.json();
-document.getElementById(
-  'crypto-name'
-).innerHTML = `<img src=${data.image.small} alt=${data.name}>
+  const data = await res.json();
+  document.getElementById(
+    'crypto-name'
+  ).innerHTML = `<img src=${data.image.small} alt=${data.name}>
 <span>${data.name}</span>
 
 `;
 
-document.getElementById('crypto-price').innerHTML = `
+  document.getElementById('crypto-price').innerHTML = `
 <p>🎯: $${data.market_data.current_price.usd.toLocaleString('en-US')}</p>
 <p>👆: $${data.market_data.high_24h.usd.toLocaleString('en-US')}</p>
 <p>👇: $${data.market_data.low_24h.usd.toLocaleString('en-US')}</p>
 `;
-
-// .catch((err) => console.error('XXX The data is unavailable', err));
+} catch (err) {
+  console.error('XXX The data is unavailable', err);
+}
 
 function getCurrentTime() {
   const now = new Date();
