891ecb27db07870461b3051e25c842409355751b
vetacode <vetacode@gmail.com>
2025-10-29 23:37:06 +0700

    removing cluttered code with  update

--- script.js
+++ script.js
@@ -127,29 +127,3 @@ async function getMyPosition(position) {
 updateWeather();
 setInterval(updateWeather, 600000);
 
-// navigator.geolocation.getCurrentPosition((position) => {
-//   fetch(
-//     `https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`
-//   )
-//     .then((res) => {
-//       if (!res.ok) {
-//         throw Error('Weather data is unavailable');
-//       }
-//       return res.json();
-//     })
-//     .then((data) => {
-//       console.log(data);
-//       const iconUrl = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-
-//       console.log(iconUrl);
-
-//       document.getElementById('weather').innerHTML = `
-//       <img src=${iconUrl} >
-//       <p>${Math.round(data.main.temp)}℃</>
-//       `;
-//       document.getElementById('city').innerHTML = `
-//       <p>${data.name}</p>
-//       `;
-//     })
-//     .catch((err) => console.error('There is something wrong', err));
-// });
